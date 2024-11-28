7c57484798cd0ffcf7eebf8dc7129b750d043ea2
Ramon <tartannut222@gmail.com>
2024-11-28 18:05:43 -0300

    variavel global dentro do scriptUrl.js

--- utils.js
+++ utils.js
@@ -68,7 +68,9 @@ function checkIfSolutionIsCorrect(fb) {
 
 function getRandExURL(userId = null, dificuldade = 0) {
     //var url = "https://script.google.com/macros/s/AKfycbwZKg-bWoZs_OgVkRUmvxxfrdQeSTWbk3lANkRDUPik-zAvLWfieRkhCgFrU415LYYg/exec?actionRequest=getExercicioAleatorio";
-    var url = "https://script.google.com/macros/s/AKfycbyPA-UzG-PVuzKK_d99wr5FS_58xsLv5yDXfIapObnJ6RD-By4EcwlX8FjUo_1sdBPp1w" + "/exec?actionRequest=getExercicioAleatorio";
+    //var url = "https://script.google.com/macros/s/AKfycbyPA-UzG-PVuzKK_d99wr5FS_58xsLv5yDXfIapObnJ6RD-By4EcwlX8FjUo_1sdBPp1w" + "/exec?actionRequest=getExercicioAleatorio";
+    
+    var url = window.scriptUrl + "/exec?actionRequest=getExercicioAleatorio"
     // Add dificuldade
     if (userId == null) {
         // NÃO FUNCIONA AINDA SEM ESTAR LOGADO
@@ -84,7 +86,9 @@ function getRandExURL(userId = null, dificuldade = 0) {
 }
 
 function getExercicioByIdURL(userId, id, dificuldade=0) {
-    var url = "https://script.google.com/macros/s/AKfycbyPA-UzG-PVuzKK_d99wr5FS_58xsLv5yDXfIapObnJ6RD-By4EcwlX8FjUo_1sdBPp1w" + "/exec?actionRequest=getExercicioById" + "&id=" + id;
+    //var url = "https://script.google.com/macros/s/AKfycbyPA-UzG-PVuzKK_d99wr5FS_58xsLv5yDXfIapObnJ6RD-By4EcwlX8FjUo_1sdBPp1w" + "/exec?actionRequest=getExercicioById" + "&id=" + id;
+    var url = window.scriptUrl + "/exec?actionRequest=getExercicioById" + "&id=" + id;
+
     if (userId == null) {
         // NÃO FUNCIONA AINDA SEM ESTAR LOGADO
         // ramon: e se setar para pegar um exercicio completamente aleatorio? sem levar em conta a dificuldade
@@ -103,9 +107,10 @@ function getExercicioByIdURL(userId, id, dificuldade=0) {
 
 function getHistoricoURL(authentication = null) {
   //  var url = "https://script.google.com/macros/s/AKfycbwZKg-bWoZs_OgVkRUmvxxfrdQeSTWbk3lANkRDUPik-zAvLWfieRkhCgFrU415LYYg/exec?actionRequest=getHistorico";
-    var url = "https://script.google.com/macros/s/AKfycbyPA-UzG-PVuzKK_d99wr5FS_58xsLv5yDXfIapObnJ6RD-By4EcwlX8FjUo_1sdBPp1w" + "/exec?actionRequest=getHistorico";
+  //  var url = "https://script.google.com/macros/s/AKfycbyPA-UzG-PVuzKK_d99wr5FS_58xsLv5yDXfIapObnJ6RD-By4EcwlX8FjUo_1sdBPp1w" + "/exec?actionRequest=getHistorico";
+    
+    var url = window.scriptUrl +"/exec?actionRequest=getHistorico";
     url = url + "&authentication=" + JSON.stringify(authentication);
-
     return url;
 }
 
